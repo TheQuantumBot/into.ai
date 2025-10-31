655632e81085257e5e61d43b44e598a152a8c789
akash-kanani <akash-kanani>
2025-10-31 21:27:28 +0530

    fe

--- src/components/Rewarding.tsx
+++ src/components/Rewarding.tsx
@@ -1,10 +1,10 @@
-
 "use client";
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import Badge from './Badge';
-import Hero from './Hero';
-import RewardingCard from './RewardingCard';
+import Badge from "./Badge";
+import Hero from "./Hero";
+import RewardingCard from "./RewardingCard";
+import Heros from "./Heros";
 
 const cards = [
   {
@@ -21,38 +21,35 @@ const cards = [
   },
 ];
 
-
 const Rewarding = () => {
-    const [current, setCurrent] = useState(0);
+  const [current, setCurrent] = useState(0);
 
-    // Auto slide every 3 seconds
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrent((prev) => (prev + 1) % cards.length);
-        }, 3000);
-        return () => clearInterval(interval);
-    }, []);
-    return (
+  // Auto slide every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % cards.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="w-full flex min-h-screen flex-col items-center justify-center py-12 text-black text-lg font-medium space-y-8 relative px-0">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <Badge text="How Its Work" />
+      </div>
 
-        <div className="w-full flex min-h-screen flex-col items-center justify-center py-12 text-black text-lg font-medium space-y-8 relative px-0">
-            <div className="px-4 sm:px-6 lg:px-8">
-                <Badge text="How Its Work" />
-            </div>
+      <div className="px-4 sm:px-6 lg:px-8 leading-[76px]">
+        <Hero
+          title={`<span class="text-black text-[60px]">Simple, Transparent, Rewarding -</span> <span class="gradient-text text-[60px]">4 Steps to Start</span>`}
+          description={
+            "Everything is automated: onboarding, tracking, and payout system."
+          }
+        />
+      </div>
+      <div className="overflow-hidden w-[320px] sm:w-[960px]">
+        <RewardingCard rewardContent={cards} current={current} />
+      </div>
+    </div>
+  );
+};
 
-            <div className="px-4 sm:px-6 lg:px-8 leading-[76px]">
-                <Hero
-                    title={`<span class="text-black text-[60px]">Simple, Transparent, Rewarding -</span> <span class="gradient-text text-[60px]">4 Steps to Start</span>`}
-                    description={
-                        "Everything is automated: onboarding, tracking, and payout system."
-                    }
-                />
-
-            </div>
-            <div className="overflow-hidden w-[320px] sm:w-[960px]">
-                <RewardingCard rewardContent={cards} current={current} />
-            </div>
-        </div>
-    )
-}
-
-export default Rewarding
+export default Rewarding;
