43092050018a0bd779d7bee47deed8b92d75f6ec
ParthivKhimani <parthiv.khimani@quantumbot.co.in>
2025-11-06 14:26:12 +0530

    Refactor Header component for improved layout, responsiveness, and styling consistency

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
         backdropFilter: "blur(6px)", 
       }}
     >
-      <div className="flex items-center justify-between px-6 py-3 ">
+      <div className="flex items-center justify-between  px-4 lg:px-6 py-3 ">
         {/* Logo */}
 
         <Link href="/">
@@ -38,7 +38,7 @@ export default function Header() {
             viewBox="0 0 142 32"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="cursor-pointer"
+            className="cursor-pointer md:w-[128px] md:h-[28px] lg:-[32px] lg:w-[142]"
           >
             <g clip-path="url(#clip0_6_757)">
               <path d="M12.0571 0H5.71338V31.9998H12.0571V0Z" fill="#E6E7E8" />
@@ -208,12 +208,12 @@ export default function Header() {
         </Link>
 
         {/* Desktop Navigation */}
-        <nav className="hidden md:flex space-x-8 text-black font-medium">
+        <nav className="hidden md:flex space-x-8 md:space-x-4 lg:space-x-8 text-black font-medium">
           {menuItems.map((item, idx) => (
             <Link
               key={idx}
               href={item.href}
-              className="relative group inline-block"
+              className="relative group inline-block md:text-[14px] text-[16px] lg:text-[16px] md:leading-[16px] leading-[20px] lg:leading-[20px] "
             >
               {item.name}
               <svg
@@ -255,14 +255,14 @@ export default function Header() {
 
         {/* CTA Button for Desktop */}
         <div className="hidden md:block">
-          <div className="flex gap-[20px]">
+          <div className="flex md:gap-[15px] lg:gap-[20px]">
             <span>
               <Link href="https://salesbot.cloud/login" target="_self">
-                <Button buttonType="white">Login</Button>
+                <Button buttonType="white" className="md:text-[14px] md:leading-[24px] lg:text-lg py-[10px] px-[20px]">Login</Button>
               </Link>
             </span>
             <span>
-              <Button>Start Free Trial</Button>{" "}
+              <Button className="md:text-[14px] md:leading-[24px] lg:text-lg min-w-[138px]">Start Free Trial</Button>{" "}
             </span>
           </div>
         </div>
