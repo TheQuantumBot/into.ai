e9c8e9fd335716082daf64a023cd01220f03116a
Dhruvil-QB <dhruvil@quantumbot.co.in>
2025-11-06 11:47:35 +0530

    change contant in FEQ

--- src/app/become-a-partner/page.tsx
+++ src/app/become-a-partner/page.tsx
@@ -8,6 +8,41 @@ import PartnerNetwork from "@/components/partnerNetwork";
 import HeroBox from "@/components/HeroBox";
 import WhatYouGet from "@/components/WhatYouGet";
 
+const faqData = [
+  {
+    id: 1,
+    question: "Do I need tech knowledge?",
+    answer1: "No. We handle all demos and setup.",
+    defaultOpen: true,
+  },
+  {
+    id: 2,
+    question: "Can I still run my business?",
+    answer1:
+      "Yes - this program is designed to run alongside your existing work.",
+    defaultOpen: true,
+  },
+  {
+    id: 3,
+    question: "Can I promote future Into AI products?",
+    answer1:
+      "Yes -  partners automatically get access to our full AI suite as we launch new tools.",
+    defaultOpen: true,
+  },
+  {
+    id: 4,
+    question: "How do I get paid?",
+    answer1: "Monthly payouts via bank transfer/UPI.",
+    defaultOpen: true,
+  },
+  {
+    id: 5,
+    question: "Is there a joining fee?",
+    answer1: "No. It’s 100% free.",
+    defaultOpen: true,
+  },
+];
+
 export default function BecameAPartnerPage() {
   return (
     <main className="min-h-screen bg-[#ECE9E6] relative overflow-hidden">
@@ -26,7 +61,7 @@ export default function BecameAPartnerPage() {
         <PartnerNetwork />
         <WhoCanBecomePartner />
         <WhatYouGet />
-        <BecameAPartnerFaq />
+        <BecameAPartnerFaq faqs={faqData as any} />
         <BecameAPartner_sales />
         <Footer />
       </div>
