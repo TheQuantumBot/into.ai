ba61caf2ac9b1d0e3436e1a8f302cd331a5f85d9
ronakp <dholariyaronak8@gmail.com>
2025-11-07 19:59:47 +0530

    merge

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    optimizePackageImports: ['react', 'react-dom']
-  }
+    optimizePackageImports: ["react", "react-dom"],
+  },
+  output: "export",
 };
 
 export default nextConfig;
-
-
