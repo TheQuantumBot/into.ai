f8ac782f9b7d0da71b4630220a86271ed97906dd
ParthivKhimani <parthiv.khimani@quantumbot.co.in>
2025-11-10 14:14:07 +0530

    Refactor Contact Us component and update background SVG for improved layout and form validation

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -6,10 +6,15 @@ export default function ContactUsPage() {
   return (
     <main className="min-h-screen bg-[#ECE9E6] relative overflow-hidden">
       {/* Content */}
+      <img
+        src="/Pricing-Bg.png"
+        alt="Background"
+        className="absolute inset-0 h-[1024px] w-[-webkit-fill-available] object-cover"
+      />
       <div className="relative z-10 min-h-screen flex flex-col">
         <Header />
 
-        <div className="flex-1 flex flex-col items-center justify-center pt-[150px]">
+        <div className="flex-1 flex flex-col items-center justify-center pt-[100px]">
           <ContactUs />
         </div>
       </div>
