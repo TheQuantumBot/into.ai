b08722aa0dcedf8952b4d440365d198ebac9f9a2
Jaimin-QB <jaimin@quantumbot.co.in>
2025-11-11 17:31:32 +0530

    fix issue

--- src/components/ROIOptimization.tsx
+++ src/components/ROIOptimization.tsx
@@ -5,17 +5,16 @@ export default function ROIOptimization() {
   return (
     <div className="bg-white rounded-3xl shadow-lg h-auto md:h-[450px] lg:h-[490px] relative overflow-hidden">
       {/* WhatsApp Icon and Chat Elements */}
-    <div className="relative h-[240px] sm:h-[300px] md:h-[345px] -mt-6 sm:-mt-8">
-          <Image
-            src="/ROIOptimization.svg"
-            alt="User 1"
-            fill
-            className="object-contain "
-          />
-        </div>
+      <div className="relative h-[240px] sm:h-[300px] md:h-[345px] -mt-6 sm:-mt-8">
+        <Image
+          src="/ROIOptimization.svg"
+          alt="User 1"
+          fill
+          className="object-contain "
+        />
+      </div>
       {/* Text Content */}
       <div className="p-4 md:p-5 lg:p-[30PX]">
-        
         <h2 className="text-[16px] leading-[20px] md:text-[20px] md:leading-[26px] lg:text-[32px] lg:leading-[40px] font-[600] text-[#1E1E1E] mb-3 sm:mb-4">
           ROI Optimization
         </h2>
