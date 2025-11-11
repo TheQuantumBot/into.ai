c42387b50bf3e99b4d211fdfb2032c8dff5baec0
Dhruvil-QB <dhruvil@quantumbot.co.in>
2025-11-11 17:18:18 +0530

    Add ScrollAnimation component and integrate it into multiple components for enhanced scroll-triggered animations

--- src/components/HomePageHeroSection.tsx
+++ src/components/HomePageHeroSection.tsx
@@ -5,6 +5,7 @@ import Button from "./Button";
 import ButtonPrimary from "./Button-primary";
 import BlurText from "./BlurText";
 import { useRouter } from "next/navigation";
+import ScrollAnimation from "./ScrollAnimation";
 
 interface HeroProps {
   title?: string | any;
@@ -27,61 +28,63 @@ export default function HomePageHeroSection({
 }: HeroProps) {
   const router = useRouter();
   return (
-    <section className="relative overflow-hidden">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-4 pb-8">
-        <div className="text-center mx-auto">
-          {/* Render Title if provided */}
+    <ScrollAnimation>
+      <section className="relative overflow-hidden">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-4 pb-8">
+          <div className="fade-up text-center mx-auto">
+            {/* Render Title if provided */}
 
-          {title && (
-            <BlurText
-              text={title}
-              delay={150}
-              animateBy="words"
-              direction="top"
-              className={`text-4xl sm:text-5xl md:text-6xl font-bold text-center leading-tight ${className}`}
-            />
-          )}
+            {title && (
+              <BlurText
+                text={title}
+                delay={150}
+                animateBy="words"
+                direction="top"
+                className={`text-4xl sm:text-5xl md:text-6xl font-bold text-center leading-tight ${className}`}
+              />
+            )}
 
-          {/* Render Description if provided */}
-          {description && (
-            <Body className="mt-2 font-inter-tight font-medium text-[18px] leading-[24px] max-w-[44rem] flex items-center justify-center mx-auto mt-[14px] align-middle !text-[#4C4C4C]">
-              {description}
-            </Body>
-          )}
+            {/* Render Description if provided */}
+            {description && (
+              <Body className="fade-up mt-2 font-inter-tight font-medium text-[18px] leading-[24px] max-w-[44rem] flex items-center justify-center mx-auto mt-[14px] align-middle !text-[#4C4C4C]">
+                {description}
+              </Body>
+            )}
 
-          {/* Buttons */}
-          {(primaryButtonText || secondaryButtonText) && (
-            <div className="flex items-center justify-center my-4 gap-2">
-              {primaryButtonText && (
-                <div>
-                  <ButtonPrimary
-                    onClick={() => {
-                      if (primaryButtonHref && primaryButtonHref != "") {
-                        router.push(primaryButtonHref);
-                      }
-                    }}
-                  >
-                    {primaryButtonText}
-                  </ButtonPrimary>
-                </div>
-              )}
-              {secondaryButtonText && (
-                <div className="flex">
-                  <Button
-                    onClick={() => {
-                      if (secondaryButtonHref && secondaryButtonHref != "") {
-                        router.push(secondaryButtonHref);
-                      }
-                    }}
-                  >
-                    {secondaryButtonText}
-                  </Button>
-                </div>
-              )}
-            </div>
-          )}
+            {/* Buttons */}
+            {(primaryButtonText || secondaryButtonText) && (
+              <div className="flex items-center justify-center my-4 gap-2">
+                {primaryButtonText && (
+                  <div>
+                    <ButtonPrimary
+                      onClick={() => {
+                        if (primaryButtonHref && primaryButtonHref != "") {
+                          router.push(primaryButtonHref);
+                        }
+                      }}
+                    >
+                      {primaryButtonText}
+                    </ButtonPrimary>
+                  </div>
+                )}
+                {secondaryButtonText && (
+                  <div className="flex">
+                    <Button
+                      onClick={() => {
+                        if (secondaryButtonHref && secondaryButtonHref != "") {
+                          router.push(secondaryButtonHref);
+                        }
+                      }}
+                    >
+                      {secondaryButtonText}
+                    </Button>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </ScrollAnimation>
   );
 }
